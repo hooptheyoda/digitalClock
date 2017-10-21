5806a7bde4634ab7a8dc074492e07b4819cdb6c2
Raven <ravenshooper@gmail.com>
2017-10-21 12:23:33 -0400

    made it work

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 var clock = document.querySelector('.clock');
+// var greet1 = document.querySelector('.greet1');
+// var greet2 = document.querySelector('.greet2');
+// var greet3 = document.querySelector('.greet3');
+
+var greet1 = document.getElementById('greet1');
+var greet2 = document.getElementById('greet2');
+var greet3 = document.getElementById('greet3');
+
+
 
 function getClock(){
   var time = new Date();
   var hrs = time.getHours();
   var min = time.getMinutes();
   var sec = time.getSeconds();
+  var ampm = ' PM ';
+
+  if (hrs < 12) {
+     ampm = ' AM ';
+  }
 
   hrs = zero(getHr(hrs));
   min = zero(min);
   sec = zero(sec);
 
- return clock.innerHTML = hrs + ':' + min + ':' + sec;
+  greeting(ampm,hrs);
+ return clock.innerHTML = hrs + ':' + min + ':' + sec + ampm;
 }
 
 function getHr(hr){
@@ -26,5 +41,27 @@ function zero(e) {
    }
    return e;
  }
+
+ function greeting(a, h) {
+   if (a === ' AM '){
+     greet1.classList.remove('hidden');
+     greet2.classList.add('hidden');
+     greet3.classList.add('hidden');
+   } else if ((a === ' PM ') && (h === 12)){
+     greet1.classList.add('hidden');
+     greet2.classList.remove('hidden');
+     greet3.classList.add('hidden');
+   } else if ((a === ' PM ') && (h < 6)){
+     greet1.classList.add('hidden');
+     greet2.classList.remove('hidden');
+     greet3.classList.add('hidden');
+   } else if ((a === ' PM ') && (h > 6)){
+     greet1.classList.add('hidden');
+     greet2.classList.add('hidden');
+     greet3.classList.remove('hidden');
+   } else {
+     console.log('uh idk');
+   }
+ } //(h < 6) || (h = 12))
 
 setInterval(getClock, 1000);
